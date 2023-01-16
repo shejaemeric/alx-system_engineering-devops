97f8b3d85773d55d8b2963a88244841243f2a63e
shejaemeric2 <shejaemeric2@gmail.com>
2023-01-16 16:08:35 +0200

    retrive data from api

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,26 +1,31 @@
+#!/usr/bin/python3
 import requests
 import sys
-import json
 
-id = sys.argv[1]
-url = "https://jsonplaceholder.typicode.com/todos/"
+def getTodos():
+    id = sys.argv[1]
+    url = "https://jsonplaceholder.typicode.com/todos/"
 
-response = requests.get('https://jsonplaceholder.typicode.com/todos')
-if response.status_code == 200:
-    results = (response).json()
-    task_completed = []
+    response = requests.get('https://jsonplaceholder.typicode.com/todos')
+    print(response.status_code)
+    if response.status_code == 200:
+        results = (response).json()
+        task_completed = []
 
-    for item in results:
-        if item['userId'] == int(id):
-            if item['completed']:
-                task_completed.append(item['title'])
+        for item in results:
+            if item.get('userId') == int(id):
+                if item.get('completed'):
+                    task_completed.append(item['title'])
 
-    print("Employee EMPLOYEE_NAME is done with tasks("
-        +str(len(task_completed))+"/"
-        +str(len(results))+"):")
+        print("Employee EMPLOYEE_NAME is done with tasks("
+            +str(len(task_completed))+"/"
+            +str(len(results))+"):")
 
-    for item in task_completed:
-        print(item)
+        for item in task_completed:
+            print(item)
 
-else:
-    print(f'incorect response')
+    else:
+        print(f'incorect response')
+
+if __name__ == "__main__":
+    getTodos()
